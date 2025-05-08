c0f4865229eab0f3b9f0e4bfed4d57256f6ce07b
claudiobentodala <programmer.claudio@gmail.com>
2025-05-08 09:53:53 +0100

    DID BASE TO MORE AND HIDE FUCTION

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,24 +1,24 @@
-var element = {
+var elements = {
     menu: window.document.querySelector('img#menu'),
     close: window.document.querySelector('img#close'),
     nav: window.document.querySelector('nav'),
     item:window.document.querySelector('div#itens')
 }
 function menu() {
-    if (element.menu.style.display == 'block') {
-        element.nav.style.display = 'flex'
-        element.menu.style.display = 'none'
-        element.close.style.display = 'block'
+    if (elements.menu.style.display == 'block') {
+        elements.nav.style.display = 'flex'
+        elements.menu.style.display = 'none'
+        elements.close.style.display = 'block'
     } else {
-        element.nav.style.display = 'none'
-        element.menu.style.display = 'block'
-        element.close.style.display = 'none'
+        elements.nav.style.display = 'none'
+        elements.menu.style.display = 'block'
+        elements.close.style.display = 'none'
     }
 }
 function out() {
-    if(element.nav.style.display = 'flex') {
-        element.nav.style.display = 'none'
-        element.menu.style.display = 'block'
-        element.close.style.display = 'none'
+    if(elements.nav.style.display = 'flex') {
+        elements.nav.style.display = 'none'
+        elements.menu.style.display = 'block'
+        elements.close.style.display = 'none'
     }
 }
