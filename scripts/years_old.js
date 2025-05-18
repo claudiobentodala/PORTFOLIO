8d8704552f1f0bff352fdd23cd4862f18dda7f6f
claudiobentodala <programmer.claudio@gmail.com>
2025-05-18 17:24:46 +0100

    DID THE YEARS OLD DINAMIC

---
+++ scripts/years_old.js
@@ -0,0 +1,15 @@
+let years_old = {
+    year: new Date().getFullYear(),
+    month: new Date().getMonth(),
+    year_born: 2009,
+    month_born: 3,
+    output: window.document.querySelector('span#years_old')
+
+}
+function years(){
+    if (years_old.month_born <= years_old.month) {
+        years_old.output.innerHTML = years_old.year - years_old.year_born 
+    } else {
+        years_old.output.innerHTML = -1 * (1 - (years_old.year - years_old.year_born )) 
+    }
+}
