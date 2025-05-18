26807d0c6c35d7c23df0faa790412683d9b63519
claudiobentodala <programmer.claudio@gmail.com>
2025-05-18 17:37:28 +0100

    GET BETTER HIDE AND MORE TO DISPLAY BLOCK AND FLEX

--- scripts/hide.js
+++ scripts/hide.js
@@ -13,7 +13,12 @@ function hide(section) {
     }
 
     if (element.content.style.display == 'none') {
-        element.content.style.display = 'block' // flex por natureza mais block por causa do ABOUT outros são flex
+        if (section == 1) {
+            element.content.style.display = 'block'
+            
+        } else {
+            element.content.style.display = 'flex'
+        }
         element.hider.style.display = 'block'
         element.more.style.display = 'none'
     } else {
