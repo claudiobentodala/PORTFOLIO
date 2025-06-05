1b6ae5100d61af96d04031c06639237ec2782beb
claudiobentodala <programmer.claudio@gmail.com>
2025-06-05 21:55:54 +0100

    separed the NUMBER TO TEXT, so use module

--- scripts/years_old.js
+++ scripts/years_old.js
@@ -1,14 +1,18 @@
 let years_old = {
-    year: new Date().getFullYear(),
-    month: new Date().getMonth(),
     year_born: 2009,
     month_born: 3,
-    output: window.document.querySelector('span#years_old')
+}
+let years_now = {
+    year: new Date().getFullYear(),
+    month: new Date().getMonth()
+}
+let output = {
+    years_old: window.document.querySelector('span#years_old')
 }
 function years(){
-    if (years_old.month_born <= years_old.month) {
-        years_old.output.innerHTML = years_old.year - years_old.year_born 
+    if (years_old.month_born <= years_now.month){
+        output.years_old.innerHTML = years_now.year - years_old.year_born 
     } else {
-        years_old.output.innerHTML = -1 * (1 - (years_old.year - years_old.year_born )) 
+        output.years_old.innerHTML = -1 * (1 - (years_now.year - years_old.year_born )) 
     }
 }
