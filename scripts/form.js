56c2df0d117e99ef3f4d965ddf4c8606db888578
claudiobentodala <programmer.claudio@gmail.com>
2025-06-06 08:24:45 +0100

    CODE BASE TO FORM LOGIC

---
+++ scripts/form.js
@@ -0,0 +1,5 @@
+let inputs = {
+    name: window.document.querySelector('input#iname'),
+    email: window.document.querySelector('input#iname'),
+    message: window.document.querySelector('input#iname')
+}
