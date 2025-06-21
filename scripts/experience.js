b682b100b3190d3ed1b95b0ede42dceaaddd9cd6
claudiobentodala <programmer.claudio@gmail.com>
2025-06-21 14:53:24 +0100

    separed function "number to text" with module

--- scripts/experience.js
+++ scripts/experience.js
@@ -1,35 +1,9 @@
+import { number_text } from "./number_to_text.js"
+
 let experience = {
     start: 2024,
     now: new Date().getFullYear()
 }
-function number_text (number){
-    if (number == 1) {
-        return"one";
-    } else  if(number== 2){
-        return"two";
-    } else  if(number== 3){
-        return"three";
-    }else  if(number== 4){
-        return"four";
-    }else  if(number== 5){
-        return"two";
-    }else  if(number== 6){
-        return"six";
-    }else  if(number== 7){
-        return"seven";
-    }else  if(number== 8){
-        return"eigth";
-    }else  if(number== 9){
-        return"nine";
-    }else  if(number== 10){
-        return"ten";
-    }else  if(number== 11){
-        return"eleven";
-    }else  if(number== 12){
-        return"twelve";
-    }else  if(number== 13){
-        return"thrteen";
-    }
-}
+console.log(number_text(3))
 
 window.document.querySelector('span#experience').innerHTML = number_text(experience.now - experience.start)
