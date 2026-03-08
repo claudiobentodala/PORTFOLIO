0da6163a91dff58391254ec864644b7703913d59
claudiobentodaladev <programmer.claudio@gmail.com>
2026-03-08 19:38:31 +0100

    deleted all the project

--- scripts/experience.js
+++
@@ -1,9 +0,0 @@
-import { number_text } from "./number_to_text.js"
-
-let experience = {
-    start: 2024,
-    now: new Date().getFullYear()
-}
-console.log(number_text(3))
-
-window.document.querySelector('span#experience').innerHTML = number_text(experience.now - experience.start)
--- scripts/form.js
+++
@@ -1,5 +0,0 @@
-let inputs = {
-    name: window.document.querySelector('input#iname'),
-    email: window.document.querySelector('input#iname'),
-    message: window.document.querySelector('input#iname')
-}
